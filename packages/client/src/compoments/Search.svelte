93bc8cb0ac887e70139a2e1fb0e4601e7a197d6e
Rob <rob@robwagner.dev>
2021-02-28 22:10:10 -0500

    Fix fetch call to proper server in production.

--- packages/client/src/compoments/Search.svelte
+++ packages/client/src/compoments/Search.svelte
@@ -3,6 +3,14 @@
     import { link, push } from 'svelte-spa-router'
     import { autocomplete, results } from '../stores'
 
+    let dbUrl = undefined
+
+    if (process.env.NODE_ENV === 'production') {
+        dbUrl = 'http://poxdb'
+    } else {
+        dbUrl = 'http://127.0.0.1:3000'
+    }
+
 
     onMount(() => {
         const searchElement = document.getElementById('search-wrapper')
@@ -68,14 +76,14 @@
 
     async function getResults() {
         const data = document.getElementById('search').value.trim()
-        await fetch(`http://127.0.0.1:3000/db?search=${data}&limit=100`)
+        await fetch(`${dbUrl}db?search=${data}&limit=100`)
             .then(res => res.json())
             .then(data => $results = data)
 
     }
 
     async function getPart(searchTerms) {
-        await fetch(`http://127.0.0.1:3000/db?search=${searchTerms}`)
+        await fetch(`${dbUrl}/db?search=${searchTerms}`)
             .then(res => res.json())
             .then(data => displayData(data))
     }
